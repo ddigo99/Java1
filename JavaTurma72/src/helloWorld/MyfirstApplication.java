6e675b9dc101db79263a34512796ba866be015d1
Rodrigo Rodrigues <rodrigonrce99@gmail.com>
2024-02-06 17:27:39 -0300

    Hello World Java

---
+++ JavaTurma72/src/helloWorld/MyfirstApplication.java
@@ -0,0 +1,13 @@
+package helloWorld;
+
+public class MyfirstApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome;
+		nome = "Rodrigo Rodrigues";
+		System.out.println("Hello world, " + nome);
+
+	}
+
+}
